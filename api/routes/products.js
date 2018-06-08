91f82047ad43b933aab435f6a9760d0e28191ffb
Raineer <delaritaraineer81@gmail.com>
2018-06-08 19:08:42 +0800

    26:39

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    });
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs)
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 router.post('/', (req, res, next) => {
      const product = new Product({
@@ -17,34 +25,32 @@ router.post('/', (req, res, next) => {
     });
     product.save().then(result => {
         console.log(result);
+        res.status(201).json({
+            message: 'Handling POST requests to /products',
+            createdProduct: result
+        });
     })
-    .catch(err => console.log(err));
-    res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+                error: err
+        });
     });
+    
 });
-// router.get('/:productId', (req, res, next) => {
-//     const id = req.params.productId;
-//     if (id === 'special') {
-//         res.status(200).json({
-//             message: 'You discovered the special ID',
-//             id: id
-//         });
-//     } else {
-//         res.status(200).json({
-//             message: 'You passed an ID'
-//         });
-//     }
-// });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
     .exec()
     .then(doc => {
-        console.log(doc);
-        res.status(200).json(doc);
+        console.log("From database", doc);
+        if(doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({message: 'No valid entry found for provided ID'});
+        }
+        
     })
     .catch(err => {
         console.log(err);
@@ -61,9 +67,7 @@ router.patch('/:productId', (req, res, next) => {
     });
 });
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted Product!'
-    });
+   
 });
 module.exports = router;
 
