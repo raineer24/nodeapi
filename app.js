11620492535df1b6ed7b160ef86d5e03a4efd1a8
Raineer <delaritaraineer81@gmail.com>
2018-06-08 18:00:26 +0800

    mongodb atlas

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.use(morgan('dev'));
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://rain:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-shard-00-00-xcn1e.mongodb.net:27017,node-rest-shop-shard-00-01-xcn1e.mongodb.net:27017,node-rest-shop-shard-00-02-xcn1e.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin',
+    {
+        useMongoClient: true
+    });
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type,Accept, Authorization"
+    );
+    if(req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods','PUT, POST, ,PATCH, DELETE, GET');
+            return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
