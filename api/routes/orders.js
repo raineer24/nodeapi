36f6bb3aa67fcf366a7444bf1bdffc6da280880b
raineer <delaritarainer81@gmail.com>
2018-06-10 18:18:37 +0800

    order not found

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -76,6 +76,11 @@ router.get('/:orderId', (req, res, next) => {
     Order.findById(req.params.orderId)
         .exec()
         .then(order => {
+            if (!order) {
+                return res.status(404).json({
+                        message: 'Order not found'
+                });
+            }
             res.status(200).json({
                 order: order,
                 request: {
@@ -91,10 +96,23 @@ router.get('/:orderId', (req, res, next) => {
         });
 });
 router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order deleted',
-        orderId: req.params.orderId
-    });
+    Order.remove({ _id: req.params.orderId })
+        .exec()
+        .then(result => {
+                       res.status(200).json({
+                message: 'Order deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/orders/',
+                    body: { productId: 'ID', quantity: "Number"}
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 
