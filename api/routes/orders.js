13e5180db6a5f482ef9ea0d10cf2a1af1398175c
raineer <delaritarainer81@gmail.com>
2018-06-10 14:46:57 +0800

    findbyId

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Order = require('../models/order');
-
+const Product = require('../models/product');
 router.get('/', (req, res, next) => {
     Order.find()
         .select('product quantity _id')
@@ -33,13 +33,17 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const order = new Order({
-        _id: mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.productId
-    });
-    order
-        .save()
+    Product.findById(req.body.productId)
+        .then(product => {
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId
+            });
+            return order.save()
+                
+              
+        })
         .then(result => {
             console.log(result);
             res.status(201).json({
