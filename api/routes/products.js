7ec52daf275108b302f6cf68333d14a955d3d077
raineer <delaritarainer81@gmail.com>
2018-06-10 22:26:08 +0800

    storing files correctly

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,22 +3,39 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Product = require('../models/product');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-// router.get('/', (req, res, next) => {
-//     Product.find()
-//         .select("name price _id")
-//         .exec()
-//         .then(docs => {
-//             console.log(docs)
-//             res.status(200).json(docs);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({
-//                 error: err
-//             });
-//         });
-// });
+
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    //reject a file
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+   
+   
+};
+
+const upload = multer({
+     storage: storage, 
+     limits: {
+    fileSize: 1024 * 1024 * 5
+},
+fileFilter: fileFilter
+ });
+
 router.get('/', (req, res, next) => {
     Product.find()
         .select("name price _id")
