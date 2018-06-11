dd0330e7ef6c739de7033b930a1afb0afd31a143
Raineer <delaritaraineer81@gmail.com>
2018-06-11 12:44:44 +0800

    JWT route protection

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,8 +5,9 @@ const mongoose = require("mongoose");
 const Order = require("../models/order");
 const Product = require("../models/product");
 
+const checkAuth = require('../middleware/check-auth');
 // Handle incoming GET requests to /orders
-router.get("/", (req, res, next) => {
+router.get("/", checkAuth,  (req, res, next) => {
     Order.find()
         .select("product quantity _id")
         .populate('product', 'name')
@@ -72,7 +73,7 @@ router.post("/", (req, res, next) => {
         });
 });
 
-router.get("/:orderId", (req, res, next) => {
+router.get("/:orderId", checkAuth,  (req, res, next) => {
     Order.findById(req.params.orderId)
         .populate('product')
         .exec()
@@ -97,7 +98,7 @@ router.get("/:orderId", (req, res, next) => {
         });
 });
 
-router.delete("/:orderId", (req, res, next) => {
+router.delete("/:orderId", checkAuth, (req, res, next) => {
     Order.remove({ _id: req.params.orderId })
         .exec()
         .then(result => {
--- api/routes/products.js
+++ api/routes/products.js
@@ -66,9 +66,8 @@ router.get('/', (req, res, next) => {
             });
         });
 });
-router.post("/", upload.single('productImage'), checkAuth,  (req, res, next) => {
-    console.log(req.file);
-    const product = new Product({
+router.post("/", checkAuth,  upload.single('productImage'),  (req, res, next) => {
+      const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price,
@@ -127,7 +126,7 @@ router.get('/:productId', (req, res, next) => {
 
 
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth,  (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for (const ops of req.body) {
@@ -151,7 +150,7 @@ router.patch('/:productId', (req, res, next) => {
             });
         })
 });
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth,  (req, res, next) => {
     const id = req.params.productId;
     Product.remove({ _id: id })
         .exec()
