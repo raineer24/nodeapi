17fe327d8d74bd3d09186dc4bbc55102f9a3971f
Raineer <delaritaraineer81@gmail.com>
2018-06-11 09:41:07 +0800

    user signup adding

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://rain:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-shard-00-00-xcn1e.mongodb.net:27017,node-rest-shop-shard-00-01-xcn1e.mongodb.net:27017,node-rest-shop-shard-00-02-xcn1e.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin',
@@ -36,6 +37,7 @@ app.use((req,res,next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
