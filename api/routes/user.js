31d5addef771b4762537dbbaa813667d997cbc98
Raineer <delaritaraineer81@gmail.com>
2018-06-12 18:01:53 +0800

    user login

--- api/routes/user.js
+++ api/routes/user.js
@@ -9,46 +9,7 @@ const UserController = require('../controllers/user');
 router.post("/signup", UserController.user_signup);
 
 
- router.post("/login", (req, res, next) => {
-     User.find({ email: req.body.email})
-     .exec()
-     .then(user => {
-         if(user.length < 1) {
-             return res.status(401).json({
-                 message: 'Auth failed'
-             });
-         }
-         bcrypt.compare(req.body.password, user[0].password,(err, result) => {
-                if(err){
-                    return res.status(401).json({
-                        message: 'Auth failed'
-                    });
-                }
-                if(result) {
-                   const token = jwt.sign({
-                        email: user[0].email,
-                        userId: user[0]._id
-                    }, process.env.JWT_KEY, {
-                        expiresIn: "1h"
-                    }
-                 );
-                    return res.status(200).json({
-                        message: 'Auth succesful',
-                        token: token
-                    });
-                }
-                res.status(401).json({
-                    message: 'Auth failed'
-                });
-         });
-     })
-     .catch(err => {
-         console.log(err);
-         res.status(500).json({
-             error: err
-         });
-     });
- });   
+ router.post("/login", UserController.user_login);   
 
 router.delete('/:userId', (req, res, next) => {
     User.remove({ _id: req.params.userId})
