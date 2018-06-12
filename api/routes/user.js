fd1d7b632e9ce9656033b69fb834cd6c819472b7
Raineer <delaritaraineer81@gmail.com>
2018-06-12 18:08:20 +0800

    auth failed checkAuth

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,11 +6,12 @@ const jwt = require('jsonwebtoken');
 
 const User = require("../models/user");
 const UserController = require('../controllers/user');
+const checkAuth = require('../middleware/check-auth');
 router.post("/signup", UserController.user_signup);
 
 
  router.post("/login", UserController.user_login);   
 
-router.delete('/:userId', UserController.user_delete) 
+router.delete('/:userId', checkAuth, UserController.user_delete) 
 
 module.exports = router;
