24b9bae9c0f7f45b7797ba53bdc6a8fd0ec7a255
Raineer <delaritaraineer81@gmail.com>
2019-03-09 19:50:10 +0800

    get orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,8 +5,15 @@ const mongoose = require('mongoose');
 const Order = require('../models/order');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
+    Order.find()
+    .exec()
+    .then(docs => {
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
