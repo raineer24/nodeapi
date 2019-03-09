b0fbe4ac41b3e939519e564b74751ea9182e8d3e
Raineer <delaritaraineer81@gmail.com>
2019-03-09 20:39:51 +0800

    overwritting orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -6,9 +6,23 @@ const Order = require('../models/order');
 
 router.get('/', (req, res, next) => {
     Order.find()
+    .select('product quantity_id')
     .exec()
     .then(docs => {
-        res.status(200).json(docs);
+        res.status(200).json({
+            count: docs.length,
+            orders: docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders/' + doc._id
+                    }
+                }
+            })
+        });
     })
     .catch(err => {
         res.status(500).json({
@@ -27,7 +41,18 @@ router.post('/', (req, res, next) => {
         .save()
         .then(result => {
             console.log(result);
-            res.status(201).json(result);
+            res.status(201).json({
+                message: 'Order stored',
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders/' + result._id
+                }
+            });
         })
         .catch( err => {
             console.log(err);
